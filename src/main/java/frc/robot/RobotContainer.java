1ad93db46494fa6561850d53d9cb7410a7334d31
Jan Bakus <jbakus@jans-mbp.lan>
2024-01-19 19:26:25 -0800

    Cleaning up the RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -41,10 +41,7 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the button bindings
     configureButtonBindings();
-
-    // Configure default commands
     m_robotDrive.setDefaultCommand(new DriveWithJoystick(m_robotDrive, m_driverController));
   }
 
