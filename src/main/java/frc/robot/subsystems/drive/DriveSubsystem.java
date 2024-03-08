073b99cffbd891bfb16fd7ea416659fc83da3cef
cosyntxd <110003521+cosyntxd@users.noreply.github.com>
2024-03-07 17:52:28 -0800

    Plan a path
    Cool new fancy tech to follow paths

--- src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -159,7 +159,7 @@ public class DriveSubsystem extends SubsystemBase {
    */
   public void drive(
       double xSpeed, double ySpeed, double rot, boolean fieldRelative, boolean rateLimit) {
-    // Preserve wheel direction when stopped, stops robot from snapping at end 
+    // Preserve wheel direction when stopped, stops robot from snapping at end
     if (Math.abs(xSpeed) < 0.01 && Math.abs(ySpeed) < 0.01 && Math.abs(rot) < 0.01) {
       modulesExecute(
           mod -> mod.setDesiredState(new SwerveModuleState(0, new Rotation2d(mod.getAngle()))));
